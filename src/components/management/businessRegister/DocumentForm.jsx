40ccdb2929476f0effef1a844fd0a56554ac1379
moon115599 <76196460+moon115599@users.noreply.github.com>
2022-09-06 16:15:53 +0900

    feat(사업자등록): 서류 첨부에서 파일 선택하면 이미지 첨부할 수 있게

--- src/components/management/businessRegister/DocumentForm.jsx
+++ src/components/management/businessRegister/DocumentForm.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Button, TextField } from "@mui/material";
 import * as Styled from "./style";
 import * as CommonStyled from "../../style";
 
 const DocumentForm = () => {
+  const businessRef = useRef();
+  const saleRef = useRef();
+
+  const [link, setLink] = useState({
+    business: "",
+    sale: "",
+  });
+  const onChangeBusiness = () => {
+    const reader = new FileReader();
+    const file = businessRef.current.files[0];
+    console.log(file.name);
+    setLink({ ...link, business: file.name });
+  };
+  const onChangeSale = () => {
+    const reader = new FileReader();
+    const file = saleRef.current.files[0];
+    setLink({ ...link, sale: file.name });
+  };
+
+  const onClickBusinessBtn = (e) => {
+    businessRef.current.click();
+  };
+  const onClickSaleBtn = (e) => {
+    saleRef.current.click();
+  };
+
   return (
     <>
       <CommonStyled.TitleDiv>
@@ -13,18 +39,53 @@ const DocumentForm = () => {
       <CommonStyled.InputsDiv>
         <CommonStyled.InputDiv>
           <span>사업자등록증</span>
-          <Button className="Button" variant="outlined">
+          <input
+            type="file"
+            className="imgInput"
+            id="logoImg"
+            accept="image/*"
+            name="file"
+            ref={businessRef}
+            onChange={onChangeBusiness}
+            style={{ display: "none" }}
+          />
+          <Button
+            className="Button"
+            variant="outlined"
+            size="small"
+            onClick={() => {
+              onClickBusinessBtn();
+            }}
+          >
             파일 선택
           </Button>
-          <input className="TextField" size="small" required variant="filled" />
+
+          <input value={link.business} className="TextField" size="small" required variant="filled" />
           <hr />
         </CommonStyled.InputDiv>
         <CommonStyled.InputDiv>
           <span>통신판매신고증</span>
-          <Button className="Button" variant="outlined">
+          <input
+            type="file"
+            className="imgInput"
+            id="logoImg"
+            accept="image/*"
+            name="file"
+            ref={saleRef}
+            onChange={onChangeSale}
+            style={{ display: "none" }}
+          />
+          <Button
+            className="Button"
+            variant="outlined"
+            size="small"
+            onClick={() => {
+              onClickSaleBtn();
+            }}
+          >
             파일 선택
           </Button>
-          <input className="TextField" size="small" required variant="filled" />
+          <input value={link.sale} className="TextField" size="small" required variant="filled" />
         </CommonStyled.InputDiv>
       </CommonStyled.InputsDiv>
     </>
