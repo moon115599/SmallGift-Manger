57edfd115afdb7b209544a8236f6f6962c6fe911
moon115599 <76196460+moon115599@users.noreply.github.com>
2022-09-29 11:04:21 +0900

    feat(api): header에 토큰 넣기

--- src/api/server/Api.js
+++ src/api/server/Api.js
@@ -1,11 +1,29 @@
 import axios from "axios";
 import { server } from "./server.js";
 import { reIssueErrorHandle, reIssueToken } from "../user/reIssueToken";
+import { useCookies } from "react-cookie";
 
-export const Api = axios.create({
+const [cookies] = useCookies([]);
+
+export const api = axios.create({
   baseURL: server,
   timeout: 10000,
   params: {},
+
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-Api.interceptors.request.use(reIssueToken, reIssueErrorHandle);
+export const accessApi = axios.create({
+  baseURL: server,
+  timeout: 10000,
+  params: {},
+
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${cookies.token}`,
+  },
+});
+
+accessApi.interceptors.request.use(reIssueToken, reIssueErrorHandle);
